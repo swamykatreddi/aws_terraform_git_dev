48503247dfc028b520b06c9838a69de46bb0bd52
swamykatreddi <katreddiswamy@gmail.com>
2025-06-25 08:56:28 +0530

    Update 1.provider.tf

--- 1.provider.tf
+++ 1.provider.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket       = "devsceops40twotwo"
+    bucket       = "shivajichatrapathi143"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
